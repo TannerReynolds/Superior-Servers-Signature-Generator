114a3cbb11f5ebad891a95b26f512249ef82f66b
Tanner <treynolds4@student.yosemite.edu>
2020-07-12 23:03:15 -0700

    Update sigs.js

--- src/server/routes/sigs.js
+++ src/server/routes/sigs.js
@@ -17,7 +17,7 @@ async function get(req, res) {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
         },
         json: true
-    }, async (err, res, body) => {
+    }, async (err, forumRes, body) => {
         if (err) return console.error(err);
 
         // Playtime >>>>>>>>>>>>>>>>>>>>>>>>>
